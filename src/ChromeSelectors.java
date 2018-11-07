0e4c5a0c7903badd2955ca96ae49ec70e7fd5929
JanisKlevs <janis.klevs@gmail.com>
2018-11-07 19:43:02 +0200

    Fourth commit

--- src/ChromeSelectors.java
+++ src/ChromeSelectors.java
@@ -1,7 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.chrome.ChromeDriverInfo;
 
-public class Demo {
+public class ChromeSelectors {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub C:\Automation\chromedriver.exe
@@ -9,12 +10,15 @@ public class Demo {
 		System.setProperty("webdriver.chrome.driver", "C:\\Automation\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver(); 
 		
-		driver.get("http://www.google.com"); //Hit url on browser
+		driver.get("http://www.facebook.com"); //Hit url on browser
+		driver.findElement(By.id("email")).sendKeys("aaa");
+		driver.findElement(By.name("pass")).sendKeys("123123");
+		driver.findElement(By.linkText("Forgot password?")).click();
 		System.out.println(driver.getTitle()); //Validate if page title is correct
 		System.out.println(driver.getCurrentUrl()); //Validate if link is correct
 		//System.out.println(driver.getPageSource()); //Print page source
-		driver.navigate().to("http://yahoo.com");
-		driver.navigate().back();
+		//driver.navigate().to("http://yahoo.com");
+		//driver.navigate().back();
 		driver.close(); //closes current browser
 		//driver.quit(); // It closes all the browsers opened by selenium script
 	}
--- src/SalesforceTests.java
+++ src/SalesforceTests.java
@@ -1,7 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.chrome.ChromeDriverInfo;
 
-public class Demo {
+public class SalesforceTests {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub C:\Automation\chromedriver.exe
@@ -9,12 +10,15 @@ public class Demo {
 		System.setProperty("webdriver.chrome.driver", "C:\\Automation\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver(); 
 		
-		driver.get("http://www.google.com"); //Hit url on browser
+		driver.get("http://login.salesforce.com"); //Hit url on browser
+		driver.findElement(By.id("username")).sendKeys("aaa");
+		driver.findElement(By.name("pw")).sendKeys("123123");
+		driver.findElement(By.className("button r4 wide primary")).click(); //Error. Compound class names not permitted
 		System.out.println(driver.getTitle()); //Validate if page title is correct
 		System.out.println(driver.getCurrentUrl()); //Validate if link is correct
 		//System.out.println(driver.getPageSource()); //Print page source
-		driver.navigate().to("http://yahoo.com");
-		driver.navigate().back();
+		//driver.navigate().to("http://yahoo.com");
+		//driver.navigate().back();
 		driver.close(); //closes current browser
 		//driver.quit(); // It closes all the browsers opened by selenium script
 	}
