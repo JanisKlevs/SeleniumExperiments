6bbfe16e6ddc759384dbd50dc0d27280abd45808
JanisKlevs <janis.klevs@gmail.com>
2018-11-07 13:25:35 +0200

    Second commit

--- src/Demo.java
+++ src/Demo.java
@@ -1,8 +1,16 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeDriverInfo;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub C:\Automation\chromedriver.exe
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Automation\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver(); 
+		
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
 
 	}
 
--- src/TestCase2.java
+++ src/TestCase2.java
@@ -1,9 +1,16 @@
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestCase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.setProperty("webdriver.gecko.driver", "C:\\Automation\\geckodriver.exe");
+		FirefoxDriver driver = new FirefoxDriver(); 
+		
+		driver.get("http://www.google.com");
+		System.out.println(driver.getTitle());
 	}
 
 }
