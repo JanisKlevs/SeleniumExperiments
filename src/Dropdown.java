b3f5c75291064cc1b6892874342ec74dcb81cbc1
JanisKlevs <janis.klevs@gmail.com>
2018-11-14 17:13:20 +0200

    this is second commit

--- src/Dropdown.java
+++ src/Dropdown.java
@@ -41,9 +41,24 @@ public class Dropdown {
 	driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_originStation1_CTXT")).click();
 	driver.findElement(By.xpath("//a[@value='GOI']")).click();
 	//driver.findElement(By.cssSelector("#ctl00_mainContent_ddl_destinationStation1_CTXT")).click();
-	//It is also works But if in code are 2 the same elements see next line how to click on second one
+	//It is also works But if in code are 2 the same elements see next line how to click on second one. Need add index in this line
 	//driver.findElement(By.cssSelector("a[value='DEL']")).click();
 	driver.findElement(By.xpath("(//a[@value='DEL'])[2]")).click();
+	//with absolute xpath
+	//driver.findElement(By.xpath("/html[1]/body[1]/div[2]/div[1]/table[1]/tbody[1]/tr[3]/td[4]/a[1]")).click();
+	//tagName[contains(@attribute,’value’)] ui-state-default ui-state-highlight ui-state-active
+	//with part of value
+	driver.findElement(By.xpath("//a[contains(@class, 'ui-state-active')]")).click();
+	try {
+		   Thread.sleep(1000);
+		} catch (InterruptedException e) {
+		   e.printStackTrace();
+		}
+	
+	System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
+	driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).click();
+	//ctl00_mainContent_chk_friendsandfamily
+	System.out.println(driver.findElement(By.id("ctl00_mainContent_chk_friendsandfamily")).isSelected());
 	
 	
 	//driver.close();
